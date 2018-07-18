fc3c6edae72abcb68fc3ee3cb386455167239d4e
Janek Schleicher <postkasten@janek-schleicher.de>
2018-07-18 13:18:52 +0200

    Create an initial connections.tf file and run terraform init.

---
+++ connections.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  credentials = "${file(".credentials/hanfried-terraform-learning-3d5ec50ce95e.json")}"
+  project = "hanfried-terraform-learning-3d5ec50ce95e"
+  region = "europe-west3"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+provider "azurerm" {
+  subscription_id = "0"
+  client_id = "1"
+  client_secret = "2"
+  tenant_id = "3"
+}
