a187752855cb650d2278bf97f0a1e359a95fb3ef
Janek Schleicher <postkasten@janek-schleicher.de>
2018-07-19 21:12:54 +0200

    Splitted resources.tf into network.tf and subnetworks.tf

---
+++ network.tf
@@ -0,0 +1,13 @@
+resource "google_compute_network" "my_dev_network" {
+  name = "devnetwork"
+  auto_create_subnetworks = false
+}
+
+resource "aws_vpc" "environment-example-2" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags {
+    Name = "terraform-learning-aws-vpc-example-2"
+  }
+}
